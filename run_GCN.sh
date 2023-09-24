107803312847529a6d0fff9f071ffc4f78ca23ec
Srikeshram <110611485+srikesh-07@users.noreply.github.com>
2023-09-24 11:10:27 +0530

    Update run_GCN.sh

--- run_GCN.sh
+++ run_GCN.sh
@@ -1,8 +1,8 @@
 GPU=0
-DATA=("PTC_MR" "IMDBBINARY" "FRANKENSTEIN")
+DATA=("DD" "PROTEINS" "PTC_MR" "IMDBBINARY" "FRANKENSTEIN")
 
 for dataset in "${DATA[@]}";
 do
-  CUDA_VISIBLE_DEVICES=${GPU} python train_gcn.py --dataset $dataset --batch_size 32
+  CUDA_VISIBLE_DEVICES=${GPU} python train_gcn.py --dataset $dataset --batch_size 32 
 done
 
